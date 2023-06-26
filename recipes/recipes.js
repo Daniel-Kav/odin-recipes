d50bcb4d4d36641f746c641b87d702e7ebeed200
daniel-kav <dankavatha01@gmail.com>
2023-06-26 11:50:14 +0300

    task 2

--- recipes/recipes.js
+++ recipes/recipes.js
@@ -2,3 +2,8 @@ const paragraph = document.createElement("p");
 paragraph.textContent = "Hey I'm Red"
 paragraph.style.color = 'red'
 document.body.appendChild(paragraph);
+
+const head = document.createElement("h3");
+head.textContent = "I'm a blue h3!";
+head.style.color ='blue';
+document.body.appendChild(head)
